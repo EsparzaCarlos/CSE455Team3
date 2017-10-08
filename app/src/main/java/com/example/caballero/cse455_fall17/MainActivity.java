ef93085efba4a048d347411ecfc1f54921399dab
CaballeroFF <CaballeroFF@users.noreply.github.com>
2017-10-07 19:29:35 -0700

    added android example
    simple hellow world android example we can use to sync

---
+++ app/src/main/java/com/example/caballero/cse455_fall17/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.caballero.cse455_fall17;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView text;
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        text = (TextView) findViewById(R.id.text);
+        button = (Button) findViewById(R.id.button);
+    }
+
+    public void onClick(View v){
+        text.setText("Hello world!");
+    }
+}
