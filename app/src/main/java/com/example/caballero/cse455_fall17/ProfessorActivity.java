80ac734c6435aa18ba2a4c4f38ba4a961c594c96
CaballeroFF <CaballeroFF@users.noreply.github.com>
2017-11-18 22:48:01 -0800

    updated main, textmanager, and professor java files

--- app/src/main/java/com/example/caballero/cse455_fall17/ProfessorActivity.java
+++ app/src/main/java/com/example/caballero/cse455_fall17/ProfessorActivity.java
@@ -1,6 +1,8 @@
 package com.example.caballero.cse455_fall17;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
@@ -20,10 +22,15 @@ import com.baoyz.swipemenulistview.SwipeMenuItem;
 import com.baoyz.swipemenulistview.SwipeMenuListView;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class ProfessorActivity extends AppCompatActivity {
 
     private static final String TAG = "ProfessorActivity";
+    TextManager txt;
+    String name = "";
+    String phone = "";
+    String email = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,9 +39,34 @@ public class ProfessorActivity extends AppCompatActivity {
 
         SwipeMenuListView listView = (SwipeMenuListView) findViewById(R.id.swipelistview);
 
+        SharedPreferences prof = getSharedPreferences("PROFESSOR_SELECT", Context.MODE_PRIVATE);
+        String professor = prof.getString("PROFESSOR", null);
+
+        final List<String> emails = new ArrayList<>();
+        emails.add("RandomProfessor@spam4me.com");
+        emails.add("Dr.Rando@spam4me.com");
+
+        final List<String> phones = new ArrayList<>();
+        phones.add("tel:0123456789");
+        phones.add("tel:9876543210");
+
         ArrayList<String> list = new ArrayList<>();
         list.add("Dr.Professor Pat");
         list.add("Dr.Professor Guy");
+
+        String repeat = "";
+        for(int i = 0; i < list.size(); i++){
+            repeat = list.get(i) + " ";
+        }
+
+        if(professor != "") {
+            getContactInfo(professor);
+            if(!txt.hasString(repeat, name)){
+                list.add(name);
+                phones.add("tel:"+phone);
+                emails.add(email);
+            }
+        }
 
         ArrayAdapter adapter = new ArrayAdapter(ProfessorActivity.this, android.R.layout.simple_list_item_1, list);
         listView.setAdapter(adapter);
@@ -79,22 +111,15 @@ public class ProfessorActivity extends AppCompatActivity {
                 switch (index) {
                     case 0:
                         Intent intent = new Intent(Intent.ACTION_DIAL);
-                        intent.setData(Uri.parse("tel:0123456789"));
+                        intent.setData(Uri.parse(phones.get(position)));
                         startActivity(intent);
                         break;
                     case 1:
-                        if(position == 0){
-                            Intent email = new Intent(Intent.ACTION_SENDTO, Uri.parse("mailto:" + "RandomProfessor@spam4me.com"));
-
-                            startActivity(Intent.createChooser(email, "Choose app to send the email"));
-                            break;
-                        }
-                        else{
-                            Intent email = new Intent(Intent.ACTION_SENDTO, Uri.parse("mailto:" + "Dr.Rando@spam4me.com"));
-
-                            startActivity(Intent.createChooser(email, "Choose app to send the email"));
-                            break;
-                        }
+                        Intent email = new Intent(Intent.ACTION_SENDTO, Uri.parse("mailto:" + emails.get(position)));
+                        startActivity(Intent.createChooser(email, "Choose app to send the email"));
+                        break;
+                    default:
+                        break;
                 }
                 // false : close the menu; true : not close the menu
                 return false;
@@ -129,6 +154,21 @@ public class ProfessorActivity extends AppCompatActivity {
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public void getContactInfo(String s) {
+        String[] array = s.split("\n");
+        name = array[0];
+
+        for (int i = 0; i < array.length; i++) {
+            android.util.Log.v("TEST", array[i]);
+            if (txt.hasString(array[i], "email")) {
+                email = array[i].substring(array[i].indexOf(" "));
+            }
+            if (txt.hasString(array[i], "phone")) {
+                phone = array[i].substring(array[i].indexOf(" "));
+            }
         }
     }
 }
--- app/src/main/java/com/example/caballero/cse455_fall17/TextManager.java
+++ app/src/main/java/com/example/caballero/cse455_fall17/TextManager.java
@@ -1,7 +1,12 @@
 package com.example.caballero.cse455_fall17;
 
 
+import android.content.Intent;
+import android.provider.ContactsContract;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.*;
+import android.util.Log;
 
 import java.text.BreakIterator;
 import java.util.ArrayList;
@@ -10,39 +15,83 @@ import java.util.Locale;
 
 public class TextManager extends AppCompatActivity {
 
+    private static String name = "";
+    private static String email = "";
+    private static String phone = "";
+
     //constructor
     public TextManager() {
     }
 
     //create list of text
-    public static List<String> populateList(List<String> list){
+    public static List<String> populateList(List<String> list, String type){
         if(!list.isEmpty())
             list.clear();
-        list.add("Exam");
-        list.add("Email");
-        list.add("midterm");
-        list.add("due");
+        switch (type){
+            case "CONTACT": list.add("Email");
+                list.add("dr");
+                list.add("phone");
+                break;
+            default:        list.add("Exam");
+                list.add("dr");
+                list.add("phone");
+                list.add("Email");
+                list.add("midterm");
+                list.add("due");
+                list.add("/");
+                break;
+        }
         return list;
     }
 
-    public static List<String> importantList(String str, List<String> list){
+    //returns list of important stuff
+    public static List<String> importantList(String str, List<String> list) {
         List<String> returnList = new ArrayList<>();
+
+        List<String> contactList = new ArrayList<>();
+        populateList(contactList, "CONTACT");
+        String contactString = "";
+        String doctor = "";
+
+        boolean next = false;
         BreakIterator iterator = BreakIterator.getSentenceInstance(Locale.US);
         iterator.setText(str);
         int start = iterator.first();
-        for(int end = iterator.next();
-            end != BreakIterator.DONE;
-            start = end, end = iterator.next()){
-            for(int j = 0; j < list.size(); j++ ){
-                String find = list.get(j);
-                if(hasString(str.substring(start,end),find)){
-                    returnList.add(str.substring(start,end));
+        for (int end = iterator.next();
+             end != BreakIterator.DONE;
+             start = end, end = iterator.next()) {
+
+            if (!hasString(str.substring(start, end), "dr.") && next) {
+                doctor = doctor + str.substring(start, end);
+                next = false;
+            }
+            if (hasString(str.substring(start, end), "dr.")) {
+                doctor = doctor + str.substring(start, end);
+                next = true;
+            }
+            for (int j = 0; j < list.size(); j++) {
+                if (hasString(str.substring(start, end), list.get(j))) {
+                    returnList.add(str.substring(start, end));
                 }
             }
         }
 
-        return returnList;
+        for (int i = returnList.size() - 1; i >= 0; i--) {
+            for (int j = contactList.size() - 1; j >= 0; j--) {
+                if (hasString(returnList.get(i), contactList.get(j))) {
+                    if (!hasString(returnList.get(i), "dr"))
+                        contactString = contactString + returnList.get(i);
+                    returnList.remove(i);
+                }
+            }
+        }
 
+        if (!contactString.isEmpty()){
+            contactString = doctor + contactString;
+            returnList.add(contactString);
+        }
+
+        return returnList;
     }
 
     //returns true if string has a given word
@@ -53,5 +102,82 @@ public class TextManager extends AppCompatActivity {
         return true;
     }
 
+    //takes in a date in mm/dd/yy format and returns a formatted date in millis
+    @NonNull
+    public static String dateDetect(String s){
+        String date = "";
+        int start = s.indexOf("/") - 2;
+        int end = s.substring(start).indexOf(" ");
+        android.util.Log.v("TXT",s.substring(start, start + end));
+        String string = s.substring(start, start + end);
+        while (hasString(string, "/")){
+            android.util.Log.v("TXT", string.substring(0, string.indexOf("/")));
+            date = date + string.substring(0, string.indexOf("/"));
+            string = string.substring(string.indexOf("/") + 1);
+            android.util.Log.v("TXT", string);
+        }
+        date = date + "20" + string + "07";
+        android.util.Log.v("TXT", date);
+        //return s.substring(start, start + end);
+        return date;
+    }
 
+    public static boolean isDate(String s){
+        if(hasString(s, "/")){
+            return true;
+            /*if(ordinalIndexOf(s, "/", 2) - ordinalIndexOf(s, "/", 1) == 3){
+                return true;
+            }*/
+        }
+        return false;
+    }
+
+    public static boolean isContact(String s){
+        if(hasString(s,"@"))
+            return true;
+        return false;
+    }
+
+    //takes in a formatted contact and returns an intent to the contacts
+    public static Intent contactDetect(String s){
+        String[] array = s.split("\n");
+        /*String name = array[0];
+        String phone = "";
+        String email = "";*/
+        name = array[0];
+
+        for(int i = 0; i < array.length; i++){
+            android.util.Log.v("TEST", array[i]);
+            if(hasString(array[i], "email")){
+                email = array[i].substring(array[i].indexOf(" "));
+            }
+            if(hasString(array[i], "phone")){
+                phone = array[i].substring(array[i].indexOf(" "));
+            }
+        }
+        Log.v("TEST", name+"---"+phone+"---"+email);
+
+        Intent contactIntent = new Intent(ContactsContract.Intents.Insert.ACTION);
+        contactIntent.setType(ContactsContract.RawContacts.CONTENT_TYPE);
+
+        contactIntent
+                .putExtra(ContactsContract.Intents.Insert.NAME, name)
+                .putExtra(ContactsContract.Intents.Insert.EMAIL, email)
+                .putExtra(ContactsContract.Intents.Insert.PHONE, phone);
+
+        return contactIntent;
+        //startActivityForResult(contactIntent, 1);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
 }
